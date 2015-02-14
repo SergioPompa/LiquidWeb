38fe1179c2d8e299387cb14e8073b6ad4c5a4c90
MacQuínez <emarquinez@gmail.com>
2015-02-14 17:11:29 +0100

    Html y Css falta JS

---
+++ js/menuMovil.js
@@ -0,0 +1,29 @@
+var altura_del_header = $('.inicio').outerHeight(true);
+var altura_del_menu_movil = $('.menu-movil').outerHeight(true);
+
+    //detectar cambios de pantalla
+    $(window).resize(cambioPantalla);
+
+    cambioPantalla();
+
+    function cambioPantalla(){
+
+      if($(window).width()<600){
+
+        $(window).scroll(function(){
+
+          posicionarMenu();
+
+        });
+      }
+    }
+
+    function posicionarMenu() {
+
+      if ($(window).scrollTop() >= altura_del_header){
+        $('.menu-movil').css('display', 'inline-block');
+        $('.wrapper').css('margin-top', (altura_del_menu_movil) + 'px');
+      }else{
+        $('.menu-movil').css('display', 'none');
+      }
+    }
