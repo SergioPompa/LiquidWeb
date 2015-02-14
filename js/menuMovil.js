2edd17251d26b13969d14881007178f5ec6bc2ea
SergioPompa <spompag@gmail.com>
2015-02-14 21:16:46 +0100

    Meter los apartados con slide en el menú movil

--- js/menuMovil.js
+++ js/menuMovil.js
@@ -1,34 +1,34 @@
 var altura_del_header = $('.inicio').outerHeight(true);
 var altura_del_menu_movil = $('.menu-movil').outerHeight(true);
 
-    //detectar cambios de pantalla
-    $(window).resize(cambioPantalla);
+//detectar cambios de pantalla
+  $(window).resize(cambioPantalla);
 
-    cambioPantalla();
+  cambioPantalla();
 
-    function cambioPantalla(){
+  function cambioPantalla(){
 
-      if($(window).width()<600){
+    if($(window).width()<600){
 
-        $(window).scroll(function(){
+      $(window).scroll(function(){
 
-          posicionarMenu();
+        posicionarMenu();
 
-        });
-      }
+      });
     }
+  }
 
-    function posicionarMenu() {
+  function posicionarMenu() {
 
-      if ($(window).scrollTop() >= altura_del_header){
-        $('.menu-movil').css('display', 'inline-block');
-        $('.wrapper').css('margin-top', (altura_del_menu_movil) + 'px');
-      }else{
-        $('.menu-movil').css('display', 'none');
-      }
+    if ($(window).scrollTop() >= altura_del_header){
+      $('.menu-movil').css('display', 'inline-block');
+      $('.wrapper').css('margin-top', (altura_del_menu_movil) + 'px');
+    }else{
+      $('.menu-movil').css('display', 'none');
     }
+  }
 
-    //función para gestionar la apertura del menú del movil
+//función para gestionar la apertura del menú del movil
 
   $('.iconoMenu').on('click', gestionaMenu);
 
@@ -36,4 +36,26 @@ var altura_del_menu_movil = $('.menu-movil').outerHeight(true);
   function gestionaMenu()
   {
     $('#menu').slideToggle("slow");
+  }
+
+//función para gestionar la apertura del menú apartado de instalaciones
+
+  $('.instalaciones').on('click', gestionaInstalaciones);
+
+
+  function gestionaInstalaciones()
+  {
+    event.preventDefault();
+
+    $('.instalacionesList').slideToggle("slow");
+    $('.instalaciones').css('background', '#F59B23').css('background', '#1F264A');
+    $('.instalaciones').on('click', salirInstalaciones);
+
+    //function salirInstalaciones()
+    //{
+    //  $('.instalaciones').css('background', '#1F264A');
+
+
+    //}
+    // event.preventDefault();
   }
