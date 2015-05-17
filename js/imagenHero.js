e7a0a8ac6e22e36d0729049e35bd953fa6fe3ec5
SergioPompa <spompag@gmail.com>
2015-05-17 11:04:45 +0200

    Ajustes para el correcto funcionamiento de la carga de la imagen y texto según la estación del año en la que estés

--- js/imagenHero.js
+++ js/imagenHero.js
@@ -5,36 +5,86 @@ function mostrarImagen() {
 	var dia = ahora.getUTCDate();
 	var mes = ahora.getUTCMonth();
 
-	if (mes>11 && dia>=22) {
+	if (mes==11) {
+		if (dia>=22) {
+			imagen="img/hero.jpg";
+			imagenMovil="img/heroMovil.jpg";
+			titular="PORQUE EL FRÍO SÓLO PUEDE SER AGRADABLE";
+			span="CUANDO ESTÁS FUERA DE CASA";
+			texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
+		} else {
+			imagen="img/otonoHero.png";
+			imagenMovil="img/otonoHeroMovil.png";
+			titular="ACONDICIONA TU HOGAR ANTES DE QUE LLEGUE";
+			span="EL FRÍO DEL INVIERNO";
+			texto="Renueve su caldera y mejore la instalación de los radiadores.";
+		}
+	} else if (mes==8) {
+		if (dia>=23) {
+			imagen="img/otonoHero.png";
+			imagenMovil="img/otonoHeroMovil.png";
+			titular="ACONDICIONA TU HOGAR ANTES DE QUE LLEGUE";
+			span="EL FRÍO DEL INVIERNO";
+			texto="Renueve su caldera y mejore la instalación de los radiadores.";
+		} else {
+			imagen="img/veranoHero.png";
+			imagenMovil="img/veranoHeroMovil.png";
+			titular="PORQUE EL CALOR SÓLO PUEDE SER AGRADABLE";
+			span="CUANDO ESTÁS FUERA DE CASA";
+			texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
+		}
+	} else if (mes==5) {
+		if (dia>=21) {
+			imagen="img/veranoHero.png";
+			imagenMovil="img/veranoHeroMovil.png";
+			titular="PORQUE EL CALOR SÓLO PUEDE SER AGRADABLE";
+			span="CUANDO ESTÁS FUERA DE CASA";
+			texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
+		} else {
+			imagen="../img/primaveraHero.png";
+			imagenMovil="../img/primaveraHeroMovil.png";
+			titular="ACONDICIONA TU HOGAR ANTES DE QUE LLEGUE";
+			span="EL CALOR DEL VERANO";
+			texto="";
+		}
+	} else if (mes==2) {
+		if (dia>=21) {
+			imagen="../img/primaveraHero.png";
+			imagenMovil="../img/primaveraHeroMovil.png";
+			titular="ACONDICIONA TU HOGAR ANTES DE QUE LLEGUE";
+			span="EL CALOR DEL VERANO";
+			texto="";
+		} else {
+			imagen="img/hero.jpg";
+			imagenMovil="img/heroMovil.jpg";
+			titular="PORQUE EL FRÍO SÓLO PUEDE SER AGRADABLE";
+			span="CUANDO ESTÁS FUERA DE CASA";
+			texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
+		}
+	} else if (mes==0 | mes==1 ) {
 		imagen="img/hero.jpg";
 		imagenMovil="img/heroMovil.jpg";
 		titular="PORQUE EL FRÍO SÓLO PUEDE SER AGRADABLE";
 		span="CUANDO ESTÁS FUERA DE CASA";
 		texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
-	} else if (mes>=8 && dia>=23) {
-		imagen="img/otonoHero.png";
-		imagenMovil="img/otonoHeroMovil.png";
+	} else if (mes==3 || mes==4 ) {
+		imagen="../img/primaveraHero.png";
+		imagenMovil="../img/primaveraHeroMovil.png";
 		titular="ACONDICIONA TU HOGAR ANTES DE QUE LLEGUE";
-		span="EL FRÍO DEL INVIERNO";
-		texto="Renueve su caldera y mejore la instalación de los radiadores.";
-	} else if (mes>=5 && dia>=21) {
+		span="EL CALOR DEL VERANO";
+		texto="";
+	} else if (mes==6 || mes==7 ) {
 		imagen="img/veranoHero.png";
 		imagenMovil="img/veranoHeroMovil.png";
 		titular="PORQUE EL CALOR SÓLO PUEDE SER AGRADABLE";
 		span="CUANDO ESTÁS FUERA DE CASA";
 		texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
-	} else if (mes>=2 && dia>=21) {
-		imagen="img/primaveraHero.png";
-		imagenMovil="img/primaveraHeroMovil.png";
+	} else if (mes==9 || mes==10 ) {
+		imagen="img/otonoHero.png";
+		imagenMovil="img/otonoHeroMovil.png";
 		titular="ACONDICIONA TU HOGAR ANTES DE QUE LLEGUE";
-		span="EL CALOR DEL VERANO";
-		texto="";
-	} else if (mes>=0 && dia>=0) {
-		imagen="img/hero.jpg";
-		imagenMovil="img/heroMovil.jpg";
-		titular="PORQUE EL FRÍO SÓLO PUEDE SER AGRADABLE";
-		span="CUANDO ESTÁS FUERA DE CASA";
-		texto="Con lo que vas a ahorrar podrás disfrutar con tu familia en lo que más te guste";
+		span="EL FRÍO DEL INVIERNO";
+		texto="Renueve su caldera y mejore la instalación de los radiadores.";
 	}
 
 
